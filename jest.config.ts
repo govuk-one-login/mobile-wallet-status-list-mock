8f12d569b3849c66f65985a1848fe70f123fa8bb
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-08-20 17:58:05 +0100

    test: update jest configuration

--- jest.config.ts
+++ jest.config.ts
@@ -4,7 +4,12 @@ export default {
     ["jest-junit", { outputDirectory: "results", outputName: "report.xml" }],
   ],
   collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**',
+  ],
   coveragePathIgnorePatterns: ["/node-modules/"],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
   preset: "ts-jest",
   testMatch: ["**/*.test.ts"],
   testEnvironment: "node",
