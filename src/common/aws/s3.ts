938e33b3cd29df2595ace84423ac1e0f5be564ae
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-08-26 20:20:00 +0100

    refactor: add explicit return type to putObject function

--- src/common/aws/s3.ts
+++ src/common/aws/s3.ts
@@ -2,7 +2,11 @@ import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 
 const s3Client = new S3Client();
 
-export async function putObject(bucket: string, key: string, body: string) {
+export async function putObject(
+  bucket: string,
+  key: string,
+  body: string,
+): Promise<void> {
   const putObjectCommand = new PutObjectCommand({
     Bucket: bucket,
     Key: key,
